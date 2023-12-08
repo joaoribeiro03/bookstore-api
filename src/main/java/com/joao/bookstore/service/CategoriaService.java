9df33ab2c35fa3cff293bda3725d178019ef791d
João Lucas Ribeiro de Moraes <joaolucasribeiro03@outlook.com>
2023-12-07 21:17:47 -0300

    excecão personalizada para objeto não encontrado

--- src/main/java/com/joao/bookstore/service/CategoriaService.java
+++ src/main/java/com/joao/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.joao.bookstore.domain.Categoria;
 import com.joao.bookstore.repositories.CategoriaRepository;
+import com.joao.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
